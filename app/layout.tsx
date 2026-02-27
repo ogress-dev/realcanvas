d159b3ca53a17c197cc837b142cbf5e389a79142
ogress-dev <ogress638@gmail.com>
2026-02-27 11:02:38 +0300

    feat: update metadata and add next-seo dependency for improved SEO management

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,67 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'David Doro',
+  metadataBase: new URL('https://dorodavid.com'),
+  title: {
+    default: 'David Doro',
+    template: '%s | David Doro'
+  },
   description: 'Brand and Product Design',
+
+  keywords:[
+    "Set Design",
+    "Industrial Design",
+    "Brand Identity",
+    "Web Design",
+    "Design Direction",
+    "UX&UI",
+    "Strategy"
+    ],
+
+    authors: [{ name: "David Doro" }],
+    creator: "David Doro",
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: 'David Doro',
+    description: 'Brand and Product Design',
+    url: 'https://dorodavid.com',
+    siteName: 'David Doro',
+    images: [
+      {
+        url: 'https://dorodavid.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'David Doro',
+    description: 'Brand and Product Design',
+    images: ['https://dorodavid.com/og-image.jpg'],
+  },
+  alternates: {
+    canonical: 'https://dorodavid.com',
+    languages: {
+      'en-US': 'https://dorodavid.com/en',
+      'es-ES': 'https://dorodavid.com/es',
+      'it': 'https://dorodavid.it',      
+      'it-IT': 'https://dorodavid.it',    
+    },
+  },
 };
 
 export default function RootLayout({
