26204e1c927fc14d9e89a0feb473624890f92ced
ogress-dev <ogress638@gmail.com>
2026-03-09 13:32:05 +0300

    chore: update dependencies in package.json
    - Added @vercel/analytics@^1.6.1
    - Added i@^0.3.7
    - Added npm@^11.11.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -151,6 +152,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
