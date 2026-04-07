6401fdd8f2e22cfa6d7b323e1c970a3861882f60
ogress-dev <ogress638@gmail.com>
2026-04-07 12:09:22 +0300

    Add Google Tag Manager integration to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,12 +150,29 @@ export default function RootLayout({
         <head>
           <meta name="google-site-verification" content="aBz2wQ2krNhgNFyVfa6Cdmwmm-4mx1wYDeZ2Fd01Hoo" />
           <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5J8PV8N2');`
+            }}
+          />
+          <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{ __html: JSON.stringify(schemas) }}
           />
         </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5J8PV8N2"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <ConvexClientProvider>
             <AdminScrollFix />
             {children}
